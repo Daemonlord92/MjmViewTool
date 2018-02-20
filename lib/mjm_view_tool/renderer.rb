b07afe5a6a6df681431ef222b075a5c8ae0275ab
3JM <zhintateo19924559@gmail.com>
2018-02-20 10:52:33 -0800

    added photo_credit feature

--- lib/mjm_view_tool/renderer.rb
+++ lib/mjm_view_tool/renderer.rb
@@ -3,5 +3,8 @@ module MjmViewTool
     def self.copyright name, msg
       "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
     end
+    def self.photo_credit name
+    	"Photo provided by <b>#{name}</b>".html_safe
+    end
   end
 end
